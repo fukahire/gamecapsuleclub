e523f6703e1dcc13d5b7cf062bf89e97483b567f
fukahire <hoy1589@gmail.com>
2025-12-02 00:47:43 +0800

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+// import HelloWorld from './components/HelloWorld.vue'
+import info from './pages/info.vue'
+</script>
+
+<template>
+  <div>
+    <info />
+  </div>
+</template>
+
+<style scoped>
+.bg{
+  background-image: './assets/bg.png';
+}
+/* .logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+} */
+</style>
---
+++ src/pages/info.vue
@@ -0,0 +1,53 @@
+<script setup lang="ts">
+// import { ref } from 'vue'
+
+// defineProps<{ msg: string }>()
+
+</script>
+
+<template>
+    <!-- 社團攤位圖 -->
+    <div class="pt-20 md:pt-0 mb-20 2xl:mb-0">
+        <div class="text-lg font-bold">
+            <p class="text-3xl font-black text-[#30507a] mb-2">遊戲膠囊康輔社</p>
+            <p>「NiCE創·迴響」遊戲主題場內Only</p>
+            <p>2026.08.15（六）</p>
+            <div class="flex flex-wrap gap-8 mt-8 h-12 justify-center items-ceters">
+                <div class="h-full"><a href="https://www.plurk.com/GameCapsuleClub" target="_blank"><img class="h-full block hover:rotate-45 rotate-0 transition border-[#e5ad55] border-2 rounded-full" src="../assets/social/fb.svg" alt=""></a></div>
+                <div class="h-full"><a href="https://www.plurk.com/GameCapsuleClub" target="_blank"><img class="h-full block hover:rotate-45 rotate-0 transition border-[#e5ad55] border-2 rounded-full" src="../assets/social/plurk.svg" alt=""></a></div>
+                <div class="h-full"><a href="https://x.com/GameCapsuleClub" target="_blank"><img class="h-full block hover:rotate-45 rotate-0 transition border-[#e5ad55] border-2 rounded-full" src="../assets/social/x.svg" alt=""></a></div>
+            </div>
+        </div>
+        <div class="flex flex-col gap-2 justify-center items-center mt-12">
+            <a href="https://forms.gle/DSy3WNjpDFzNKa3Z8" class="bg-[#30507a] hover:bg-[#e5ad55] border-3 rounded-full py-2 font-bold text-white border-white w-80 block">活動參與意願調查</a>
+            <a href="https://starstonetw.weebly.com/" class="bg-[#30507a] hover:bg-[#e5ad55] border-3 rounded-full py-2 font-bold text-white border-white w-80 block">NiCE 創·迴響2相關資訊</a>
+        </div>
+        <div class="w-full flex justify-center items-center">
+            <div class="flex gap-2 justify-items-center justify-center items-center mt-12 flex-wrap">
+                <img src="../assets/sprite/dante.png" alt="" class="block h-20 md:h-32">
+                <img src="../assets/sprite/felyne.png" alt="" class="block h-20 md:h-32">
+                <img src="../assets/sprite/leon.png" alt="" class="block h-20 md:h-32">
+                <img src="../assets/sprite/luke.png" alt="" class="block h-20 md:h-32">
+                <img src="../assets/sprite/naruhodo.png" alt="" class="block h-20 md:h-32">
+                <img src="../assets/sprite/mitsurugi.png" alt="" class="block h-20 md:h-32">
+                <img src="../assets/sprite/odoroki.png" alt="" class="block h-20 md:h-32">
+                <img src="../assets/sprite/naruhodo_acient.png" alt="" class="block h-20 md:h-32">
+                <img src="../assets/sprite/nero.png" alt="" class="block h-20 md:h-32">
+                <img src="../assets/sprite/netto.png" alt="" class="block h-20 md:h-32">
+                <img src="../assets/sprite/vergil.png" alt="" class="block h-20 md:h-32">
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+/* .container{
+    width:100%
+} */
+.read-the-docs {
+  color: #888;
+}
+a{
+    color: white;
+}
+</style>
