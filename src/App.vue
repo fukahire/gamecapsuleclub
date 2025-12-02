1f55ee800a94384444edc5be90172aee77edd561
fukahire <hoy1589@gmail.com>
2025-12-03 01:22:39 +0800

    fix3

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 // import HelloWorld from './components/HelloWorld.vue'
-// import info from './pages/info.vue'
+import info from './pages/info.vue'
 </script>
 
 <template>
