4a64c039441ca8704b3345bca14d03f56a563cab
fukahire <hoy1589@gmail.com>
2026-03-22 17:00:59 +0800

    lang

--- src/pages/minigame.vue
+++ src/pages/minigame.vue
@@ -8,19 +8,39 @@
     // 題目位置，要寫題目產生器
     const questList = reactive([
         {
-            'hint':'洛克人英文怎麼拼',
+            hint: {
+            ch: '洛克人日文名的英文怎麼拼',
+            en: 'How to spell Mega Man (in Romaji)?',
+            ko: '록맨의 영어 스펠링이 어떻게 되나요?',
+            jp: 'ロックマンのローマ字綴りは何ですか？',
+        },
             'letter':'ROCKMAN',
             'mapimg':['0', '0', '0', '0', '1', '2', '3', '0', '0', '0', '4', '5', '0', '0', '7', '6'],
         },{
-            'hint':'學園風的洛克人exe是什麼型態',
+            hint: {
+                ch: '學園風的洛克人EXE是什麼型態',
+                en: 'What is the Style of Mega Man EXE in character page(in Romaji)?',
+                ko: '캐릭터 페이지에 있는 록맨 EXE의 스타일은 무엇인가요?',
+                jp: 'キャラクターページにおけるロックマンEXEのスタイルは何ですか？',
+            },
             'letter':'NUMBERSOUL',
             'mapimg':['0','0','0','1','0','5','4','3','2','0','6','7','0','0','0','0','8','0','0','0','0','9','10','0','0'],
         },{
-            'hint':'可以改變洛克人exe 0.001%基因資料的修正檔是什麼',
+            hint: {
+                ch: '可以改變洛克人EXE 0.001%基因資料的修正檔是什麼',
+                en: 'What is the patch file that can change 0.001% of Mega Man EXE\'s genetic data(in Romaji)?',
+                ko: '록맨 EXE의 유전자 데이터를 0.001% 변경할 수 있는 수정 파일은 무엇인가요?',
+                jp: 'ロックマンEXEの遺伝子データを0.001%変更できる修正パッチは何ですか？',
+            },
             'letter':'SAITOBAT',
             'mapimg':['0','0','0','0','8','3','2','1','7','4','0','0','6','5','0','0'],
         },{
-            'hint':'熱斗跟洛克人exe意識完全重合、操作沒有時間差的狀態是什麼',
+            hint: {
+                ch: '熱斗跟洛克人EXE意識完全重合、操作沒有時間差的狀態是什麼',
+                en: 'What is the state where Lan and Mega Man EXE reach full synchronization with no lag (in Romaji)?',
+                ko: '넷토와 록맨 EXE의 의식이 완전히 일치하고 조작 지연이 없는 상태는 무엇인가요?',
+                jp: '熱斗とロックマンEXEの意識が完全に同調し、操作のタイムラグがない状態を何といいますか？',
+            },
             'letter':'FULLSYNCHRO',
             'mapimg':['0','0','8','9','0','0','6','7','10','11','0','5','4','0','0','1','2','3','0','0','0','0','0','0','0'],
         },
@@ -139,6 +159,47 @@
         cube.active = !cube.active
         check(cube.letter)
     }
+
+
+    const currentlang = ref('ch')
+    const langChange = function(e:string){
+        currentlang.value = e
+    }
+
+    const langMap = ref([
+    { id: 'ch', name: '中文' },
+    { id: 'en', name: 'English' },
+    { id: 'ko', name: '한국어' },
+    { id: 'jp', name: '日本語' }
+]);
+
+   const reStartMap = ref([
+    { id: 'ch', name: '重新開始' },
+    { id: 'en', name: 'RESTART' },
+    { id: 'ko', name: '다시 시작' },
+    { id: 'jp', name: 'はじめから' }
+])
+
+const changeQuestMap = ref([
+    { id: 'ch', name: '換題目' },
+    { id: 'en', name: 'NEXT QUIZ' }, // 或是 'NEXT Q'
+    { id: 'ko', name: '문제 변경' },
+    { id: 'jp', name: '問題を変更' }
+])
+
+const currentHint = computed(()=>{
+    const hint = currentQuest.value.hint
+    const lang = currentlang.value
+    const reStart = reStartMap.value.find(item => item.id === lang)
+    const changeQuest = changeQuestMap.value.find(item => item.id === lang)
+    
+    return {
+        hint:hint[lang]
+        ,reStart:reStart?.name
+        ,changeQuest:changeQuest?.name
+    }
+    })
+
 </script>
 
 <template>
@@ -146,11 +207,16 @@
     <div class="flex flex-col justify-center items-center">
         <div class="font-bold mb-4 w-full gap-4 max-w-[40rem] grid grid-cols-6 justify-center items-center h-[5rem] p-4 ring-[#e5ad55] ring-4 rounded-lg bg-white">
             <div class="relative"><img class="max-w-[5rem] absolute top-1/2 left-1/2 w-full -translate-x-1/2 -translate-y-1/2" src="/chatimg/1230.png" alt=""></div>
-            <p class="col-span-5">{{ currentQuest.hint }}</p>
+            <p class="col-span-5">{{ currentHint.hint }}</p>
         </div>
-        <div class="flex gap-2 mb-8">
-            <p @click="changeQuestion()" class="bg-[#30507a] text-white p-2 rounded cursor-pointer font-bold">換題目</p>
-            <p @click="reset()" class="bg-[#30507a] text-white p-2 rounded cursor-pointer font-bold">重新開始</p>
+        <div class="flex justify-between gap-20 items-center mb-8">
+            <div class="flex gap-2">
+                <p @click="changeQuestion()" class="bg-[#30507a] text-white p-2 rounded cursor-pointer font-bold">{{ currentHint.changeQuest }}</p>
+                <p @click="reset()" class="bg-[#30507a] text-white p-2 rounded cursor-pointer font-bold">{{ currentHint.reStart }}</p>
+            </div>
+            <div class="flex gap-1">
+                <p  class="bg-[#30507a] text-white p-2 rounded cursor-pointer font-bold" v-for="items in langMap" :key="items.id" @click="langChange(items.id)">{{ items.name }}</p>
+            </div>
         </div>
         <div :class="`grid gap-1 ${cubeNumber} w-full max-w-[40rem]`">
             <minigameBtn :info="cube.letter" :active="cube.active" :gamestatus="gameStatus"  v-for="(cube,index) in Qarr" :key="`${cube}${index}`" @click="tab(cube)"/>
